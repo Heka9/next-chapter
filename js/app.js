0d0ce16dab7988eb33e23c16d4cb7f6c5317d730
Heka9 <90552593+Heka9@users.noreply.github.com>
2023-01-24 15:35:36 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
 (() => {
     "use strict";
+    function isWebp() {
+        function testWebP(callback) {
+            let webP = new Image;
+            webP.onload = webP.onerror = function() {
+                callback(2 == webP.height);
+            };
+            webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+        }
+        testWebP((function(support) {
+            let className = true === support ? "webp" : "no-webp";
+            document.documentElement.classList.add(className);
+        }));
+    }
     let bodyLockStatus = true;
     let bodyLockToggle = (delay = 500) => {
         if (document.documentElement.classList.contains("lock")) bodyUnlock(delay); else bodyLock(delay);
@@ -56,5 +69,6 @@
         }
     }), 0);
     window["FLS"] = false;
+    isWebp();
     menuInit();
 })();
